a229e226b76a10ffb8f13afb056f08c24b3a2e17
apair00 <32197921+apair00@users.noreply.github.com>
2024-03-04 15:35:17 -0600

    Add files via upload

---
+++ Debug/configPkg/package/cfg/empty_pem4f.h
@@ -0,0 +1,34 @@
+/*
+ *  Do not modify this file; it is automatically 
+ *  generated and any modifications will be overwritten.
+ *
+ * @(#) xdc-B06
+ */
+
+#include <xdc/std.h>
+
+#include <ti/sysbios/hal/Hwi.h>
+extern const ti_sysbios_hal_Hwi_Handle halHwi0;
+
+#include <ti/sysbios/hal/Hwi.h>
+extern const ti_sysbios_hal_Hwi_Handle halHwi1;
+
+#include <ti/sysbios/knl/Swi.h>
+extern const ti_sysbios_knl_Swi_Handle swi0;
+
+#include <ti/sysbios/knl/Task.h>
+extern const ti_sysbios_knl_Task_Handle task0;
+
+#include <ti/sysbios/knl/Task.h>
+extern const ti_sysbios_knl_Task_Handle task1;
+
+#include <ti/sysbios/knl/Semaphore.h>
+extern const ti_sysbios_knl_Semaphore_Handle Semaphore0;
+
+#include <ti/sysbios/knl/Semaphore.h>
+extern const ti_sysbios_knl_Semaphore_Handle Semaphore1;
+
+extern int xdc_runtime_Startup__EXECFXN__C;
+
+extern int xdc_runtime_Startup__RESETFXN__C;
+
